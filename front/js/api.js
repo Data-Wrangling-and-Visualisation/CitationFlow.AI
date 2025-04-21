1a5e5f50ca8247e971991b028ea61fbffe97d91d
LowIQCoder <llamaetoia@yandex.ru>
2025-04-21 12:35:03 +0300

    fix

--- front/js/api.js
+++ front/js/api.js
@@ -1,7 +1,9 @@
 const BASE_URL = "http://localhost:8000";
 
 export async function getNodes() {
-  const response = await fetch(`${BASE_URL}/nodes`);
+  const response = await fetch(`${BASE_URL}/nodes`, {
+    credentials: "include",
+  });
   if (!response.ok) {
     throw new Error("Failed to fetch nodes");
   }
